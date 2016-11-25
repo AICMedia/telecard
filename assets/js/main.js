cf785c922ca75db7172ed7e0e5f43c1478d1484f
Anastasia Barybina <ab@aic.ru>
2016-11-25 12:19:04 +0300

    Новая версия
    Signed-off-by: Anastasia Barybina <ab@aic.ru>

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,10 @@
 
 (function($) {
 
+	var	$window = $(window),
+		$body = $('body'),
+		$banner = $('#banner');
+
 	skel.breakpoints({
 		wide: '(max-width: 1680px)',
 		normal: '(max-width: 1280px)',
@@ -15,60 +19,59 @@
 		smobile: '(max-width: 480px)'
 	});
 
-	$(function() {
-		var	$window = $(window),
-			$body = $('body'),
-			$banner = $('#banner');
 
-		$window.on('load', function() {
+		
+
+		$(window).on('load', function() {
 			$(".preloader").fadeOut(600);
 			$('#header .inner').addClass("animated fadeInDown");
 		});
+		
+		var isiPad = navigator.userAgent.match(/iPad/i) != null;
+	    if ((navigator.platform.indexOf("iPhone") != -1) || (navigator.platform.indexOf("iPod") != -1)){
+	    	window.location.replace("https://appsto.re/ru/lbWV6.i");
+	    }
 
-		var isMobile = false; //initiate as false
-		// device detection
-	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-		if ( isMobile ) {
- 			
-		}
+	    if (/Android/i.test(navigator.userAgent)) {
+    		window.location.replace("https://play.google.com/store/apps/details?id=ru.bpc.mobilebank.android");
+  		}
 
 	//Скрипт меню и вывода блоков
 		function activeBlock(){
-		var a_block = $("#nav").find(".active").attr('data-category');
-		$('#banner .description').each(function(){
-			var c_block = $(this);
-			var sh_block = $(this).attr('data-category');
+			var $a_block = $("#nav").find(".active").data('category');
+			$('#banner .description').each(function(){
+				var $c_block = $(this);
+				var $sh_block = $(this).data('category');
 
-			if(a_block == sh_block){
-				$(c_block).toggleClass("fadeInRight animated");
-				setTimeout(function(){
-					$(c_block).show();
-				},200);
-			} else{
-				$(c_block).removeClass("fadeInRight");
-				setTimeout(function(){
-					$(c_block).hide().removeClass("animated");
-				},200); 
-			} 
-		});
+				if($a_block == $sh_block){
+					$c_block.toggleClass("fadeInRight animated");
+					setTimeout(function(){
+						$c_block.show();
+					},200);
+				} else{
+					$c_block.removeClass("fadeInRight");
+					setTimeout(function(){
+						$c_block.hide().removeClass("animated");
+					},200); 
+				} 
+			});
 
-		$('.image img').each(function(){
-			var c_block = $(this);
-			var sh_block = $(this).attr('data-category');
+			$('.phone img').each(function(){
+				var $c_block = $(this);
+				var $sh_block = $(this).data('category');
 
-			if(a_block == sh_block){
-				$(c_block).toggleClass("fadeIn animated");
-				setTimeout(function(){
-					$(c_block).show();
-				},200);
-			} else{
-				$(c_block).removeClass("fadeIn");
-				setTimeout(function(){
-					$(c_block).hide().removeClass("animated");
-				},200); 
-			} 
-		});
+				if($a_block == $sh_block){
+					$c_block.toggleClass("fadeIn animated");
+					setTimeout(function(){
+						$c_block.show();
+					},200);
+				} else{
+					$c_block.removeClass("fadeIn");
+					setTimeout(function(){
+						$c_block.hide().removeClass("animated");
+					},200); 
+				} 
+			});
 		
 		}
 
@@ -94,15 +97,11 @@
 		      // itemsTablet: true,
 
 			});
-			$('.owl-page').last().hide();
-			$('.download').parent().hide();
 			$('.owl-carousel').trigger('resize.owl.carousel');
 			$('.item').show();	
-			$('.download-section').show();
 			
 		} else {
 			activeBlock();
-			$('.download-section').hide();
 		}
 
 		var owl = $('.owl-carousel');
@@ -110,17 +109,13 @@
 			if ($('body').outerWidth() > 737) {
 				$('#owl-demo').removeClass('owl-theme');
 				activeBlock();
-				$('.download-section').hide();
-				$('.download').parent().show().removeClass('owl-item');
 			} else {
 				$('#owl-demo').addClass('owl-theme');
 				owl.trigger('resize.owl.carousel');
 				console.log($('.item').attr('class'));
 				$('.item').show();	
-				$('.download').parent().hide().removeClass('owl-item');
-				$('.download-section').show();
+				$('#banner').reload();
 			}
-			$('.owl-page').last().hide();
 		});
 		
 
@@ -136,6 +131,39 @@
 
 		$('.scrolly').scrolly(); // Scrolly кнопки
 
-	});
+		function setHeiHeight() {
+			if ($('body').outerWidth() > 737){
+				$('#header').css({
+		        	height: $(window).height() - ($(window).height() / 4.5) + 'px'
+		    	});
+			} else {
+				$('#header').css({
+		        	height: $(window).height() - ($(window).height() / 8) + 'px'
+		    	});
+			}
+		    
+
+		    $('#banner').css({
+		        height: $(window).height() + 'px'
+		    });
+		    $('.preloader').css({
+		        height: $(window).height() + 'px'
+		    });
+		    if ($('body').outerWidth() > 737) {
+			    $('.owl-wrapper-outer').css({
+			        width: ($(window).width() / 3)+ 'px'
+			    });
+			} else if ($('body').outerWidth() > 480){
+				 $('.owl-wrapper-outer').css({
+			        width: ($(window).width() / 2)+ 'px'
+			    });
+			} else {
+				$('.owl-wrapper-outer').css({
+			        width: ($(window).width() / 1.4)+ 'px'
+			    });
+			}
+		}
+		setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+		$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
 
 })(jQuery);
